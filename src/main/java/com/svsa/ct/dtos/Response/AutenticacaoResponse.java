4e64084a8d8b4328188cef5d28464bbb927baed1
fravss <anaflaviamartins56@gmail.com>
2025-02-17 09:53:37 -0300

    adicionar model mapper

---
+++ src/main/java/com/svsa/ct/dtos/Response/AutenticacaoResponse.java
@@ -0,0 +1,12 @@
+package com.svsa.ct.dtos.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AutenticacaoResponse {
+    private String token;
+}
